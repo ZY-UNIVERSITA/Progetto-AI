1dcd0d368d3f510bfee37794533419ad8c49b7d2
ZY <yuhang.zhu@studio.unibo.it>
2025-05-28 19:00:15 +0200

    Separation of image loading function

--- preprocessing/_01_image_crop.py
+++ preprocessing/_01_image_crop.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 from utils.read_file import load_json
 import os
+from utils.load_image import imread_unicode
 
 # ------------------------------------------------------------------
 # PARAMETRI DA PERSONALIZZARE
@@ -46,38 +47,6 @@ class ImageCrop:
         self.input: str = os.path.join(self.general_dir, cfg[DATA_SETTINGS][DIRECTORY][INPUT_DIR])
         self.output: str = os.path.join(self.general_dir, cfg[DATA_SETTINGS][DIRECTORY][OUTPUT_DIR])
 
-    def imread_unicode(
-        self, path: str, flags: int = cv2.IMREAD_GRAYSCALE
-    ) -> cv2.typing.MatLike:
-        """
-        Prova a leggere il file con imread -> semplice e veloce.\n
-        Se fallisce, usa imdecode che funziona sempre.
-        """    
-    
-        # prova lettura con imgread
-        try:
-            path.encode("ascii")  
-            img = cv2.imread(path, flags)
-
-            if img is not None:
-                return img
-        except UnicodeEncodeError as e:
-            print("Errore nella lettura del file per incompatabilità di caratteri non ASCII. Caricamento dei file in altra modalità.")
-        
-        # Se fallisce, come accade solitamente con caratteri non ASCII
-        # usa lettura binaria con imdecode
-        try:
-            with open(path, "rb") as f:
-                byte_data: bytes = f.read()
-
-            data = np.frombuffer(byte_data, dtype=np.uint8)
-            img = cv2.imdecode(data, flags)
-
-            return img
-        except Exception as e:
-            print(f"Impossibile leggere il file {path}: errore {e}")
-            return None
-
     def save_cell(self, cell_img, dest_path: str) -> bool:
         """
         Salva una cella su disco.\n
@@ -110,7 +79,7 @@ class ImageCrop:
         out_dir.mkdir(parents=True, exist_ok=True)
 
         # 1) Caricamento immagine
-        img = self.imread_unicode(
+        img = imread_unicode(
             str(img_path), cv2.IMREAD_COLOR if self.grey else cv2.IMREAD_GRAYSCALE
         )
 
---
+++ utils/load_image.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+def imread_unicode(
+    self, path: str, flags: int = cv2.IMREAD_GRAYSCALE
+) -> cv2.typing.MatLike:
+    """
+    Prova a leggere il file con imread -> semplice e veloce.\n
+    Se fallisce, usa imdecode che funziona sempre.
+    """    
+
+    # prova lettura con imgread
+    try:
+        path.encode("ascii")  
+        img = cv2.imread(path, flags)
+
+        if img is not None:
+            return img
+    except UnicodeEncodeError as e:
+        print("Errore nella lettura del file per incompatabilità di caratteri non ASCII. Caricamento dei file in altra modalità.")
+    
+    # Se fallisce, come accade solitamente con caratteri non ASCII
+    # usa lettura binaria con imdecode
+    try:
+        with open(path, "rb") as f:
+            byte_data: bytes = f.read()
+
+        data = np.frombuffer(byte_data, dtype=np.uint8)
+        img = cv2.imdecode(data, flags)
+
+        return img
+    except Exception as e:
+        print(f"Impossibile leggere il file {path}: errore {e}")
+        return None
