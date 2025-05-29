5b8cb683907d58673f89a0f20538581c7e0c8eb0
ZY <yuhang.zhu@studio.unibo.it>
2025-05-29 19:47:50 +0200

    Add visual utils class

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -1,7 +1,8 @@
 from .TrainerCNN import TrainerCNN
+from .Training_engine import TrainingEngine
 
 # Interfaccia pubblica di quello esportabile
-__all__ = ["TrainerCNN"]
+__all__ = ["TrainerCNN", "TrainingEngine"]
 
 # Codice di inizializzazione
 print("Il package dei trainer è stato importato!")
---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+from .visual_util import ColoredPrint
+
+# Interfaccia pubblica di quello esportabile
+__all__ = ["ColoredPrint"]
+
+# Codice di inizializzazione
+print("Il package degli utils è stato importato!")
