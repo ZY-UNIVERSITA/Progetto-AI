e3df775469f5f16ec7ffbfce146af64f41af892a
ZY <yuhang.zhu@studio.unibo.it>
2025-05-29 12:19:43 +0200

    Add data augmentation function

---
+++ preprocessing/_03_data_augmentation.py
@@ -0,0 +1,214 @@
+import os
+from torchvision.transforms import v2 as transforms
+from pathlib import Path
+import cv2
+import torch
+
+from torchvision.utils import save_image
+
+from utils.load_image import imread_unicode
+
+# ------------------------------------------------------------------
+# PARAMETRI DA PERSONALIZZARE
+# ------------------------------------------------------------------
+GENERAL_SETTINGS: str = "general_settings"
+DATA_AUG: str = "data_aug"
+SEED: str = "seed"
+
+DATA_SETTINGS: str = "data_settings"
+DIRECTORY: str = "directory"
+GENERAL_DIR: str = "general_dir"
+DATASET_DIR: str = "dataset_dir"
+
+DATASET: str = "dataset"
+TRAIN_DIR: str = "train_dir"
+TRAIN: str = "train"
+TRAIN_DIR_AUG: str = "train_dir_aug"
+
+DATA_AUGMENTATION: str = "data_augmentation"
+RANDOM_AFFINE: str = "random_affine"
+RANDOM_PERSPECTIVE: str = "random_perspective"
+RANDOM_ELASTIC: str = "random_elastic"
+RANDOM_COLOR_JITTER: str = "random_color_jitter"
+RANDOM_SHARPNESS: str = "random_sharpness"
+RANDOM_APPLY: str = "random_apply"
+RANDOM_CONTRAST: str = "random_contrast"
+RANDOM_EQUALIZE: str = "random_equalize"
+RANDOM_ERASING: str = "random_erasing"
+COPIES: str = "copies"
+
+EXTS = {".jpg", ".jpeg", ".png", ".bmp", ".tif", ".tiff"}
+
+
+# ------------------------------------------------------------------
+# CLASSE DI DATA AUGMENTATION
+# ------------------------------------------------------------------
+class DataAugmentation:
+    def __init__(self, cfg: dict) -> None:
+        self.cfg: dict = cfg
+
+        self.toAug: bool = cfg[GENERAL_SETTINGS][DATA_AUG]
+
+        self.seed: int = cfg[GENERAL_SETTINGS][SEED]
+        torch.manual_seed(self.seed)
+        torch.cuda.manual_seed_all(self.seed)
+
+        self.general_dir: str = cfg[DATA_SETTINGS][DIRECTORY][GENERAL_DIR]
+
+        self.input: str = os.path.join(
+            self.general_dir,
+            cfg[DATA_SETTINGS][DIRECTORY][DATASET_DIR],
+            cfg[DATA_SETTINGS][DATASET][TRAIN_DIR],
+        )
+
+        self.output: str = os.path.join(
+            self.general_dir,
+            cfg[DATA_SETTINGS][DIRECTORY][DATASET_DIR],
+            cfg[DATA_SETTINGS][DATASET][TRAIN_DIR_AUG],
+        )
+
+        self.copies: int = cfg[DATA_AUGMENTATION][COPIES]
+
+        self.randomAffine: bool = cfg[DATA_AUGMENTATION][RANDOM_AFFINE]
+        self.randomProspective: bool = cfg[DATA_AUGMENTATION][RANDOM_PERSPECTIVE]
+        self.randomElastic: bool = cfg[DATA_AUGMENTATION][RANDOM_ELASTIC]
+        self.randomColorJitter: bool = cfg[DATA_AUGMENTATION][RANDOM_COLOR_JITTER]
+        self.randomSharpness: bool = cfg[DATA_AUGMENTATION][RANDOM_SHARPNESS]
+        self.randomApply: bool = cfg[DATA_AUGMENTATION][RANDOM_APPLY]
+        self.randomContrast: bool = cfg[DATA_AUGMENTATION][RANDOM_CONTRAST]
+        self.randomEqualize: bool = cfg[DATA_AUGMENTATION][RANDOM_EQUALIZE]
+        self.randomErasing: bool = cfg[DATA_AUGMENTATION][RANDOM_ERASING]
+
+        self.transform_list: list = []
+        self.createTransformationPipeline()
+        self.compose = transforms.Compose(self.transform_list)
+
+    def createTransformationPipeline(self) -> None:
+        # Convert image to tensor
+        tensor = transforms.ToImage()
+        self.transform_list.append(tensor)
+        tensor_float = transforms.ToDtype(torch.float32, scale=True)
+        self.transform_list.append(tensor_float)
+
+        # Apporta modifiche alla geometria dell'immagine, come rotazione, traslazione, scala e inclinazione
+        if self.randomAffine:
+            affine = transforms.RandomApply(
+                [
+                    transforms.RandomAffine(
+                        degrees=10,
+                        translate=(0.05, 0.05),
+                        scale=(0.9, 1.1),
+                        shear=(-5, 5, -5, 5),
+                        # interpolation=transforms.InterpolationMode.NEAREST,
+                        fill=(255, 255, 255),
+                    )
+                ],
+                p=1,
+            )
+
+            self.transform_list.append(affine)
+
+        # Deforma l'immagine simulando una prospettiva diversa
+        if self.randomProspective:
+            perspective = transforms.RandomPerspective(
+                distortion_scale=0.2, p=0.4, fill=(255, 255, 255)
+            )
+
+            self.transform_list.append(perspective)
+
+        # Applica una trasformazione elastica (water-like-effect) per distorcere l'immagine
+        if self.randomElastic:
+            elastic = transforms.ElasticTransform(
+                alpha=30.0, sigma=4.0, p=0.2, fill=(255, 255, 255)
+            )
+
+            self.transform_list.append(elastic)
+
+        # Modifica la luminosità e il contrasto dell'immagine
+        if self.randomColorJitter:
+            color_jitter = transforms.RandomApply(
+                [
+                    transforms.ColorJitter(
+                        brightness=0.2,
+                        contrast=0.2,
+                    )
+                ],
+                p=0.3,
+            )
+
+            self.transform_list.append(color_jitter)
+
+        # Regola la nitidezza dell'immagine
+        if self.randomSharpness:
+            sharpness = transforms.RandomAdjustSharpness(sharpness_factor=2, p=0.3)
+
+            self.transform_list.append(sharpness)
+
+        # Applica della sfocatura
+        if self.randomApply:
+            gaussianBlur = transforms.RandomApply(
+                [transforms.GaussianBlur(kernel_size=3)], p=0.3
+            )
+
+            self.transform_list.append(gaussianBlur)
+
+        # Modifica il contrasto
+        if self.randomContrast:
+            contrast = transforms.RandomAutocontrast(p=0.2)
+
+            self.transform_list.append(contrast)
+
+        # Bilancia la distribuzione delle intensità dei pixel
+        if self.randomEqualize:
+            equalize = transforms.RandomEqualize(p=0.2)
+
+            self.transform_list.append(equalize)
+
+        # Aggiunge del rumore, con parti tagliate
+        if self.randomErasing:
+            erasing = transforms.RandomErasing(
+                scale=(0.01, 0.04),
+                ratio=(0.5, 2.0),
+                value="random",
+                p=0.2,
+                inplace=False,
+            )
+
+            self.transform_list.append(erasing)
+
+    def process_image(self, image: Path, output_dir: str, base_name: str) -> None:
+        # Legge l'immagine
+        orig_image = imread_unicode(str(image))
+
+        # Crea delle copie e applica data augmentation e salva l'immagine
+        for i in range(self.copies):
+            aug_image: torch.Tensor = self.compose(orig_image)
+
+            save_image(aug_image, str(output_dir / f"{base_name}_aug_{i}.jpg"))
+
+    def augmentation(self) -> bool:
+        if not self.toAug:
+            return False
+
+        print("\nINIZIO DATA AUGMENTATION\n" + "-" * 20)
+
+        input: Path = Path(self.input)
+
+        for class_dir in input.iterdir():
+            images: list[Path] = [path for path in class_dir.iterdir()]
+
+            # Nome della directory a seconda del carattere
+            dir_name: str = class_dir.name
+
+            # Crea la directory per il char
+            output_dir = Path(self.output) / dir_name
+            output_dir.mkdir(parents=True, exist_ok=True)
+
+            # for image in images:
+            for image in images:
+                base_name: str = image.name.split(".")[0]
+                self.process_image(image, output_dir, base_name)
+
+        print("\nFINE DATA AUGMENTATION\n" + "-" * 20)
+
+        return True
--- train.py
+++ train.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from utils.read_file import load_json
 from preprocessing._01_image_crop import ImageCrop
 from preprocessing._02_dataset_split import DatasetSplit
+from preprocessing._03_data_augmentation import DataAugmentation
 
 class Train:
     def __init__(self, cfg: str):
@@ -11,12 +12,16 @@ class Train:
     
         self.crop_image: ImageCrop = ImageCrop(self.cfg)
         self.split_dataset: DatasetSplit = DatasetSplit(self.cfg)
+        self.data_augmentation: DataAugmentation = DataAugmentation(self.cfg)
         
     def cropImage(self):
         self.crop_image.cropImage()
 
     def slitDataset(self):
         self.split_dataset.splitDataset()
+
+    def dataAugmentation(self):
+        self.data_augmentation.augmentation()
 
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
@@ -26,3 +31,4 @@ if __name__ == "__main__":
     train = Train(Path(args.cfg))
     train.cropImage()
     train.slitDataset()
+    train.dataAugmentation()
