1a75834f403310e12bd44c725833bbc98afbd747
ZY <yuhang.zhu@studio.unibo.it>
2025-05-29 12:20:04 +0200

    Update load_image.py

--- utils/load_image.py
+++ utils/load_image.py
@@ -1,24 +1,25 @@
 import cv2
 import numpy as np
 
-def imread_unicode(
-    self, path: str, flags: int = cv2.IMREAD_GRAYSCALE
-) -> cv2.typing.MatLike:
+
+def imread_unicode(path: str, flags: int = cv2.IMREAD_GRAYSCALE) -> cv2.typing.MatLike:
     """
     Prova a leggere il file con imread -> semplice e veloce.\n
     Se fallisce, usa imdecode che funziona sempre.
-    """    
+    """
 
     # prova lettura con imgread
     try:
-        path.encode("ascii")  
+        path.encode("ascii")
         img = cv2.imread(path, flags)
 
         if img is not None:
             return img
     except UnicodeEncodeError as e:
-        print("Errore nella lettura del file per incompatabilità di caratteri non ASCII. Caricamento dei file in altra modalità.")
-    
+        print(
+            "Errore nella lettura del file per incompatabilità di caratteri non ASCII. Caricamento dei file in altra modalità."
+        )
+
     # Se fallisce, come accade solitamente con caratteri non ASCII
     # usa lettura binaria con imdecode
     try:
