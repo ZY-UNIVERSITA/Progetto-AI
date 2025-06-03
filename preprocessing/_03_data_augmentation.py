c957d70999864b6bb3f17357a344782d99796fed
ZY <yuhang.zhu@studio.unibo.it>
2025-06-03 21:39:54 +0200

    Fix img resize

--- preprocessing/_03_data_augmentation.py
+++ preprocessing/_03_data_augmentation.py
@@ -24,6 +24,8 @@ TRAIN_DIR: str = "train_dir"
 TRAIN: str = "train"
 TRAIN_DIR_AUG: str = "train_dir_aug"
 
+IMG_SIZE: str = "img_size"
+
 DATA_AUGMENTATION: str = "data_augmentation"
 RANDOM_AFFINE: str = "random_affine"
 RANDOM_PERSPECTIVE: str = "random_perspective"
@@ -68,6 +70,7 @@ class DataAugmentation:
 
         self.copies: int = self.cfg[DATA_AUGMENTATION][COPIES]
 
+        self.img_size: int = self.cfg[DATA_SETTINGS][IMG_SIZE]
         self.randomAffine: bool = self.cfg[DATA_AUGMENTATION][RANDOM_AFFINE]
         self.randomProspective: bool = self.cfg[DATA_AUGMENTATION][RANDOM_PERSPECTIVE]
         self.randomElastic: bool = self.cfg[DATA_AUGMENTATION][RANDOM_ELASTIC]
@@ -79,8 +82,6 @@ class DataAugmentation:
         self.randomErasing: bool = self.cfg[DATA_AUGMENTATION][RANDOM_ERASING]
 
         self.transform_list: list = []
-        self.createTransformationPipeline()
-        self.compose = transforms.Compose(self.transform_list)
 
     def createTransformationPipeline(self) -> None:
         # Convert image to tensor
@@ -88,6 +89,11 @@ class DataAugmentation:
         self.transform_list.append(tensor)
         tensor_float = transforms.ToDtype(torch.float32, scale=True)
         self.transform_list.append(tensor_float)
+
+        # Resize images
+        resize = transforms.Resize((self.img_size, self.img_size))
+        self.transform_list.append(resize)
+
 
         # Apporta modifiche alla geometria dell'immagine, come rotazione, traslazione, scala e inclinazione
         if self.randomAffine:
@@ -196,9 +202,14 @@ class DataAugmentation:
 
         print("\nINIZIO DATA AUGMENTATION\n" + "-" * 20)
 
+        self.createTransformationPipeline()
+        self.compose = transforms.Compose(self.transform_list)
+
         input: Path = Path(self.input)
 
         for class_dir in input.iterdir():
+            print(f"\nInizio data augmentation per il carattere {class_dir.name}")
+
             images: list[Path] = [path for path in class_dir.iterdir()]
 
             # Nome della directory a seconda del carattere
@@ -212,6 +223,8 @@ class DataAugmentation:
             for image in images:
                 base_name: str = image.name.split(".")[0]
                 self.process_image(image, output_dir, base_name)
+
+            print("Fine data augmentation.\n")
 
         print("\nFINE DATA AUGMENTATION\n" + "-" * 20)
 
