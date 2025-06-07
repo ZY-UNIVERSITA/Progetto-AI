948a86230c4d03170c32d0fc81a8d98ddf93eff1
ZY <yuhang.zhu@studio.unibo.it>
2025-06-07 15:41:47 +0200

    Add load json to init file

--- utils/__init__.py
+++ utils/__init__.py
@@ -3,9 +3,10 @@ from .Early_stopping import EarlyStopping
 from .Metrics import Metrics
 from .config_enum import ConfigKeys
 from .LR_scheduler import LR_scheduler
+from .read_file import load_json as load_JSON
 
 # Interfaccia pubblica di quello esportabile
-__all__ = ["ColoredPrint", "EarlyStopping", "Metrics", "ConfigKeys", "LR_scheduler"]
+__all__ = ["ColoredPrint", "EarlyStopping", "Metrics", "ConfigKeys", "LR_scheduler", "load_JSON"]
 
 # Codice di inizializzazione
 print("Il package degli utils è stato importato!")
