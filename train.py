6b104900dbc2b561eee483d2c0de8b414641b00d
ZY <yuhang.zhu@studio.unibo.it>
2025-06-07 15:42:37 +0200

    Update main entry point to support dynamic model

--- train.py
+++ train.py
@@ -1,13 +1,14 @@
 import argparse
 from pathlib import Path
 
-from utils.read_file import load_json
+from utils import load_JSON
 from preprocessing import ImageCrop, DatasetSplit, DataAugmentation
-from trainer import TrainerCNN, InferenceCNN
+from trainer import TrainerCNN, TesterCNN
 
 class Train():
-    def __init__(self, cfg: str):
-        self.cfg: dict = load_json(cfg)
+    def __init__(self, cfg: str, model_config: str = None):
+        self.cfg: dict = load_JSON(cfg)
+        self.mode_config = load_JSON(model_config)
     
         self.crop_image: ImageCrop = ImageCrop(self.cfg)
         self.split_dataset: DatasetSplit = DatasetSplit(self.cfg)
@@ -23,7 +24,7 @@ class Train():
         self.data_augmentation.augmentation()
 
     def loadTrainer(self):
-        self.trainer = TrainerCNN(self.cfg)
+        self.trainer = TrainerCNN(self.cfg, self.mode_config)
 
     def trainLog(self):
         self.trainer.loggingInfo()
@@ -32,7 +33,7 @@ class Train():
         self.trainer.train()
 
     def loadInference(self):
-        self.inferenceClass = InferenceCNN(self.cfg)
+        self.inferenceClass = TesterCNN(self.cfg)
 
     def inference(self):
         self.inferenceClass.inference()
@@ -40,16 +41,17 @@ class Train():
 if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("--cfg", required=True, help="config.json")
+    ap.add_argument("--cfg_model", required=False, help="model_config.json")
     args = ap.parse_args()
 
-    train = Train(Path(args.cfg))
-    # train.cropImage()
-    # train.slitDataset()
-    # train.dataAugmentation()
+    train = Train(Path(args.cfg), Path(args.cfg_model))
+    train.cropImage()
+    train.slitDataset()
+    train.dataAugmentation()
 
-    # train.loadTrainer()
-    # train.trainLog()
-    # train.train()
+    train.loadTrainer()
+    train.trainLog()
+    train.train()
 
     train.loadInference()
     train.inference()
