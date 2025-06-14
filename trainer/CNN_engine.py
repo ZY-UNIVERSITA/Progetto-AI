512ff78b7775557b35867f971d861efeca954db5
ZY <yuhang.zhu@studio.unibo.it>
2025-06-14 10:52:15 +0200

    Refactor engine

--- trainer/CNN_engine.py
+++ trainer/CNN_engine.py
@@ -9,12 +9,31 @@ from utils import ColoredPrint, Metrics
 
 from torch.optim.lr_scheduler import LRScheduler
 
+from torch.utils.tensorboard import SummaryWriter
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+from sklearn.metrics import confusion_matrix
+
 TRAIN: str = "train"
 VAL: str = "val"
 TEST: str = "test"
 
+
 class CNNEngine:
-    def __init__(self, model: nn.Module, loss, optimizer, scheduler: LRScheduler, scheduler_type: str, device: str, classes: int, writer = None):
+    def __init__(
+        self,
+        model: nn.Module,
+        loss,
+        optimizer,
+        scheduler: LRScheduler,
+        scheduler_type: str,
+        device: str,
+        classes: dict[str, int],
+    ):
         self.model: nn.Module = model
         self.loss = loss
         self.optimizer = optimizer
@@ -23,13 +42,16 @@ class CNNEngine:
         self.device = device
         self.classes = classes
 
+    def add_writer(self, writer):
+        self.writer: SummaryWriter = writer
+
     def exec_epoch(self, dataLoader: DataLoader, tqdm_desc: str) -> Tuple[float, float]:
         # Variabili per calcolare loss e accuracy
         total_loss: float = 0.0
         correct: int = 0
         total: int = 0
 
-        train: bool = (tqdm_desc == TRAIN)
+        train: bool = tqdm_desc == TRAIN
 
         # Training mode:
         # train: dropout e batch normalization
@@ -47,7 +69,7 @@ class CNNEngine:
 
         # Carica i dati per batch
         for images, labels in progress_bar:
-            
+
             # Spostamento delle immagini e etichette sul device di elaborazione (CPU o GPU)
             images, labels = images.to(self.device), labels.to(self.device)
 
@@ -60,7 +82,7 @@ class CNNEngine:
             # Disabilitato se è in test/val/inferenza
             with torch.set_grad_enabled(train):
                 # Calcola le predizioni per tutte le immagini
-                outputs = self.model(images)    
+                outputs = self.model(images)
 
                 # Calcolare la perdita per tutte le immagini
                 loss = self.loss(outputs, labels)
@@ -98,13 +120,54 @@ class CNNEngine:
             real_y = real_y + labels.tolist()
             pred_y = pred_y + preds.tolist()
 
-        mt = Metrics(self.classes, real_y, pred_y)
+        # mt = Metrics(self.classes, real_y, pred_y)
 
-        mt.report()
+        # mt.report()
+
+        # mt.compute_confusion_matrix()
+
+        # matrix = mt.confusion_matrix
+
+        if not train:
+            # trasforma gli indici in stringhe tramite il dizionario
+            index_to_label = {v: k for k, v in self.classes.items()}
+
+            # crea una lista di stringhe
+            labels = list(index_to_label.keys())
+        
+            # crea la matrice di confusione
+            cm = confusion_matrix(real_y, pred_y, labels=labels)
+
+            # Crea una heatmap con le label 
+            fig, ax = plt.subplots(figsize=(8, 6))
+            sns.heatmap(
+                cm,
+                annot=True,
+                fmt="d",
+                cmap="Greens",
+                xticklabels=[index_to_label[i] for i in labels],
+                yticklabels=[index_to_label[i] for i in labels],
+            )
+
+            # impostazione delle label degli assi
+            plt.xlabel("Predicted Label")
+            plt.ylabel("True Label")
+            plt.title("Confusion Matrix")
+
+            # rotazione delle etichette dell'asse x per una migliore leggibilità
+            plt.xticks(rotation=0)
+            plt.yticks(rotation=0)
+
+            # salva un immagine della matrice
+            plt.savefig("confusion_matrix.png", dpi=300)
+
+            # Aggiunta della figura a TensorBoard
+            self.writer.add_figure("Matrice di confusione finale", fig, global_step=0)
+
+            plt.close()
 
         # calcola la loss e l'accuracy
         avg_loss: float = total_loss / total
         avg_acc: float = 100 * correct / total
-        
 
         return avg_loss, avg_acc
