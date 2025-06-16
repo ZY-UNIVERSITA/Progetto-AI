c95f1876a9586817103fc35fde66bbd0d2d8c59a
ZY <yuhang.zhu@studio.unibo.it>
2025-06-16 12:24:15 +0200

    Clean unused import

--- models/CNN/__init__.py
+++ models/CNN/__init__.py
@@ -1,6 +1,4 @@
 from .v00_cnn_semplice import SmallCNN as CNN_v0
-from .v01_cnn_semplice import SmallCNN as CNN_v1
-from .v03_cnn_semplice import SmallCNN as CNN_v3
 from .CNN_model import FlexibleCNNModel as CNNModel
 
-__all__ = ["CNN_v0", "CNN_v1", "CNN_v3", "CNNModel"]
+__all__ = ["CNN_v0", "CNNModel"]
